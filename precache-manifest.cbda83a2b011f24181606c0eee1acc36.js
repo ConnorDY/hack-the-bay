5f10084be4c013e344b29458597c32fd10b4d98d
Connor Younglund <cdy@airmail.cc>
2020-08-23 16:38:15 -0400

    Updates

---
+++ precache-manifest.cbda83a2b011f24181606c0eee1acc36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561b36e11ead517366afd5eab62524a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f78b15f01db878a1f7e",
+    "url": "/static/js/2.58f48600.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.58f48600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c42c5bd94341b352c895",
+    "url": "/static/js/main.4de62f95.chunk.js"
+  },
+  {
+    "revision": "01a104d15681264f3d58",
+    "url": "/static/js/runtime-main.b149833b.js"
+  }
+]);

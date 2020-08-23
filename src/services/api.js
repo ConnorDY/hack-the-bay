46ea7c082a21c8fb58a52cf9807c85b9020504ac
Connor Younglund <cdy@airmail.cc>
2020-08-23 15:28:54 -0400

    add endpoint for grabbing a range of years' data

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import flatten from 'lodash.flatten';
 
 export async function getDataMap() {
   return (await axios.get(`./data/map.json`)).data;
@@ -7,4 +8,14 @@ export async function getDataMap() {
 export async function getData(fips, year, parameter) {
   return (await axios.get(`./data/${fips}/${year}/${parameter}/data.json`))
     .data;
+}
+
+export async function getDataForYears(fips, parameter, yearStart, yearEnd) {
+  const yearPromises = [];
+  for (let year = yearStart; year <= yearEnd; year++) {
+    yearPromises.push(getData(fips, year, parameter));
+  }
+
+  const years = await Promise.all(yearPromises);
+  return flatten(years);
 }
